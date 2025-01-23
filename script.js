cd6f601471598bbe7c9ce4fe4ef4685c11da09e8
Danilo Divjak <137777658+danilodivjak@users.noreply.github.com>
2025-01-23 15:01:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function menuOpen(){
+    const menu = document.querySelector(".mobile-menu");
+    menu.style.display = "flex";
+}
+function menuClose(){
+    const menu = document.querySelector(".mobile-menu");
+    menu.style.display = "none";
+}
